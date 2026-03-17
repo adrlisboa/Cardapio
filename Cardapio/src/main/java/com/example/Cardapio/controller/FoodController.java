e92bc3afe85621511d401d7334d305dfd73838f7
Adriano <adriano.psv@hotmail.com>
2026-03-17 16:44:23 -0300

    first commit

---
+++ Cardapio/src/main/java/com/example/Cardapio/controller/FoodController.java
@@ -0,0 +1,28 @@
+package com.example.Cardapio.controller;
+
+
+import com.example.Cardapio.food.Food;
+import com.example.Cardapio.food.FoodRepository;
+import com.example.Cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("com/example/Cardapio/food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @GetMapping
+    public List<FoodResponseDTO> getAll() {
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+
+    }
+}
