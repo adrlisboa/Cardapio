455a9e753a3775549dab0fff55b3a183bb31d6d8
Adriano <adriano.psv@hotmail.com>
2026-03-19 16:11:52 -0300

    FoodController ajustado

--- Cardapio/src/main/java/com/example/Cardapio/controller/FoodController.java
+++ Cardapio/src/main/java/com/example/Cardapio/controller/FoodController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("com/example/Cardapio/food")
+@RequestMapping("/food")
 public class FoodController {
 
     @Autowired
